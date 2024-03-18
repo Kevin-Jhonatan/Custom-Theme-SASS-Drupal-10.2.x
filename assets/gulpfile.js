ec90488f2686b83f2b337f3da94d5b1b10ecb918
Kevin-Jhonatan <kevin7.world@gmail.com>
2024-03-18 01:10:48 -0400

    Update

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,19 +1,22 @@
 // Ponemos las dependencias de gul y gulp-sass
 var gulp = require("gulp");
+
 const sass = require("gulp-sass")(require("sass"));
 
 // Automatizamos la compresion de css
 
 gulp.task("sass", (done) => {
-  gulp
-    .src("./scss/**/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }))
-    .pipe(gulp.dest("./css"));
+  
+  gulp.src("./scss/**/*.scss").pipe(sass({ outputStyle: "compressed" })).pipe(gulp.dest("./css"));
+  
   done();
+
 });
 
 gulp.task("watch", (done) => {
+  
   gulp.watch("./scss/**/*.scss").on("change", gulp.series("sass"));
   
   done();
+  
 });
